fd1ecbaaaf28f5d31c706d0fd97e32e2561e7679
Saju Thankappan <sajuthankappan@gmail.com>
2020-06-24 18:59:52 +0000

    Model name changes as in sync with worker

--- src/data/repositories/audit_detail_repository.rs
+++ src/data/repositories/audit_detail_repository.rs
@@ -1,14 +1,14 @@
-use crate::models::UrlAuditDetail;
+use crate::models::AuditDetail;
 use wread_data_mongodb::crud_repository;
 use wread_data_mongodb::mongodb::bson::oid::ObjectId;
 use wread_data_mongodb::mongodb::{error::Error, Database};
 
-const COLLECTION_NAME: &str = "urlAuditDetails";
+const COLLECTION_NAME: &str = "auditDetails";
 
 pub async fn get_by_id(
     id: &String,
     db: &Database,
-) -> Result<Option<UrlAuditDetail>, Error> {
+) -> Result<Option<AuditDetail>, Error> {
     let object_id = ObjectId::with_string(id.as_str()).unwrap();
     crud_repository::find_by_id(&object_id, COLLECTION_NAME, &db).await
 }
