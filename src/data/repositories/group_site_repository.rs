8e95455e206b6b5d6bfc75d42d9568b7e8c5b672
Saju Thankappan <sajuthankappan@gmail.com>
2020-09-17 17:54:18 +0530

    Sort by siteName for groupSites list

--- src/data/repositories/group_site_repository.rs
+++ src/data/repositories/group_site_repository.rs
@@ -8,6 +8,7 @@ const COLLECTION_NAME: &str = "groupSites";
 
 pub async fn get_by_group_id(id: &String, db: &Database) -> Result<Vec<GroupSite>, Error> {
     let object_id = ObjectId::with_string(id.as_str()).unwrap();
-    let doc = doc! { "groupId": object_id };
-    crud_repository::find(doc, COLLECTION_NAME, &db).await
+    let filter_doc = doc! { "groupId": object_id };
+    let sort_doc = doc! { "siteName": 1 };
+    crud_repository::find_with_sort(filter_doc, sort_doc, COLLECTION_NAME, &db).await
 }
